68bd1adfd0b0eae12fb4bb5c22b1235a2cb55aeb
VinYuvian <53811990+VinYuvian@users.noreply.github.com>
2021-02-18 16:50:11 +0530

    Update s3_backend.tf

--- terraform/s3_backend.tf
+++ terraform/s3_backend.tf
@@ -4,8 +4,8 @@ terraform {
     bucket         = "vin1711-s3-backend"
     key            = "global/s3/terraform.tfstate"
     region         = "ap-south-1"
-    access_key     = $access_key
-    secret_key     = $secret_key
+    #access_key     = $access_key
+    #secret_key     = $secret_key
     # Replace this with your DynamoDB table name!
     dynamodb_table = "s3-backend-lock"
     encrypt        = true
