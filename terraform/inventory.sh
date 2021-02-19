8badec6870c1997d7d67fdf2069513d97588e9af
VinYuvian <53811990+VinYuvian@users.noreply.github.com>
2021-02-19 13:14:44 +0530

    Update inventory.sh

--- terraform/inventory.sh
+++ terraform/inventory.sh
@@ -2,7 +2,7 @@
 server="$2"
 type="$1"
 if [ $type == "[masters]" ];then
-	sed -i "/^\[masters\]/a $server" ../ansible/hosts/inventory
+	sed -i "/^\[masters\]/a $server" inventory
 elif [ $type == "[workers]" ];then
-	sed -i "/^\[workers\]/a $server" ../ansible/hosts//inventory
+	sed -i "/^\[workers\]/a $server" inventory
 fi
