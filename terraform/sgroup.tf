6f0e3dc11b5d42bc8a188c26bdf915eff1037cd3
VinYuvian <53811990+VinYuvian@users.noreply.github.com>
2021-02-24 12:14:19 +0530

    Update sgroup.tf

--- terraform/sgroup.tf
+++ terraform/sgroup.tf
@@ -17,6 +17,15 @@ resource "aws_security_group_rule" "allow_all" {
     cidr_blocks = ["10.240.0.0/16"]
 }
 
+resource "aws_security_group_rule" "allow_some_kubernetes_access" {
+    type = "ingress"
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    security_group_id = aws_security_group.kubernetes-securitygroup.id
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "allow_ssh" {
     type = "ingress"
     from_port = 22
