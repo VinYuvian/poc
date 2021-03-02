0f1e32d0cae24b8b7ddc4c1d252bc2abc2b12b7e
VinYuvian <53811990+VinYuvian@users.noreply.github.com>
2021-03-02 09:48:39 +0530

    Update workers.tf

--- terraform/workers.tf
+++ terraform/workers.tf
@@ -1,10 +1,11 @@
 # K8s Worker Instances
 
 resource "aws_instance" "worker" {
-    count = 2
+    count = var.worker_count
+    subnet_id=count.index+1>length(tolist(data.aws_subnet_ids.public.ids))?tolist(data.aws_subnet_ids.public.ids)[count.index%2]:tolist(data.aws_subnet_ids.public.ids)[count.index]
     ami = var.ami
     instance_type = var.worker_instance_type
-    subnet_id="${tolist(data.aws_subnet_ids.public.ids)[count.index]}"
+    #subnet_id=tolist(data.aws_subnet_ids.public.ids)[count.index]
     #subnet_id = data.aws_subnet_ids.public.ids[count.index]
     #private_ip = cidrhost("${var.public[count.index]}/24", 30 + count.index)
     associate_public_ip_address = true # Instances have public, dynamic IP
@@ -13,10 +14,6 @@ resource "aws_instance" "worker" {
     #availability_zone = "${var.region}${var.az[count.index]}"
     vpc_security_group_ids = [aws_security_group.kubernetes-securitygroup.id]
     key_name = "kubernetes-poc"
-    provisioner "local-exec"{
-			command="/bin/bash inventory.sh '[workers]' 'worker${count.index} ansible_host=${self.public_ip}'"
-	  }
-
     tags = {
       Name = "worker-${count.index}"
       ansibleNodeType = "worker"
