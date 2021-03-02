3c07a5bdb0e61cb07355cbb8c4ea0d2188f80272
VinYuvian <53811990+VinYuvian@users.noreply.github.com>
2021-03-02 09:47:37 +0530

    Update controllers.tf

--- terraform/controllers.tf
+++ terraform/controllers.tf
@@ -1,25 +1,22 @@
 resource "aws_instance" "controller" {
-    count = "1"
+    count = var.master_count
     ami = var.ami
     instance_type = var.controller_instance_type
 
     iam_instance_profile = aws_iam_instance_profile.kubernetes.id
-    subnet_id="${tolist(data.aws_subnet_ids.public.ids)[count.index]}"
+    subnet_id=tolist(data.aws_subnet_ids.public.ids)[count.index]
     #subnet_id = data.aws_subnet_ids.public[count.index]
-    #private_ip = cidrhost("${var.public[count.index]}/24", 20 + count.index)
+    private_ip = var.master_pvt_ip
     associate_public_ip_address = true # Instances have public, dynamic IP
     source_dest_check = false # TODO Required??
 
     #availability_zone = "${var.region}${var.az[count.index]}"
-    vpc_security_group_ids = ["${aws_security_group.kubernetes-securitygroup.id}"]
+    vpc_security_group_ids = [aws_security_group.kubernetes-securitygroup.id]
     key_name = "kubernetes-poc"
-    provisioner "local-exec"{
-			command="/bin/bash inventory.sh '[masters]' 'master ansible_host=${self.public_ip}'"
-	  }
-
     tags = {
       Name = "controller-${count.index}"
       ansibleNodeType = "controller"
-      ansibleNodeName = "controller${count.index}"
+      k8s-control-plane = ""
+      "kubernetes.io/cluster/kubernetes" = "owned"
     }
 }
