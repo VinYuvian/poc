79cf6c3fcd04ba782d5dc0e11525a1955d7f6d44
VinYuvian <53811990+VinYuvian@users.noreply.github.com>
2021-03-04 16:10:49 +0530

    Update alarm.tf

--- terraform/alarm.tf
+++ terraform/alarm.tf
@@ -1,17 +1,29 @@
+data "aws_sns_topic" "example" {
+  name = "test-poc"
+  depends_on = [
+      aws_cloudformation_stack.tf_sns_topic
+  ]
+}
+
 resource "aws_cloudwatch_metric_alarm" "cpu" {
   count  = var.worker_count
-  alarm_name                = "web-cpu-alarm"
+  //count = length(data.aws_instances.worker.ids)
+  alarm_name                = "web-cpu-alarm${count.index}"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
+  evaluation_periods        = "2"
   metric_name               = "CPUUtilization"
   namespace                 = "AWS/EC2"
   period                    = "120"
   statistic                 = "Average"
   threshold                 = "80"
   alarm_description         = "This metric monitors ec2 cpu utilization"
-  alarm_actions             = [ aws_sns_topic.alarm.arn ]
+  alarm_actions             = [ data.aws_sns_topic.example.arn ]
 
   dimensions = {
+    //InstanceId = each.key
     InstanceId = aws_instance.worker[count.index].id
   }
+  depends_on = [
+      aws_instance.worker
+  ]
 }
